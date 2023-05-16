e3b10d518ed6c97efd7b0ef65638ba4c8c3c3439
kkoemna <115250897+kkoemna@users.noreply.github.com>
2023-05-16 17:40:46 +0900

    Bladeの利用

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -7,8 +7,8 @@ use Illuminate\Http\Response;
 
 class HelloController extends Controller
 {
-    public function index(Request $request) {
-        $data = ['msg'=>'これはコントローラから渡されたメッセージです。', 'id'=>$request->id];
+    public function index() {
+        $data = ['msg'=>'これはBladeを利用したサンプルです。'];
         return view('hello.index', $data);
     }
 }
